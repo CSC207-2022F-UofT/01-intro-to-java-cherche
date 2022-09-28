74f51c6590694fde00990ef05a98525df0a17516
Ryan Nguyen <ry.nguyen@mail.utoronto.ca>
2022-09-28 13:29:52 -0400

    Complete tasks

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -63,6 +64,7 @@ public class Basics {
          *                value 100.
          */
 
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -100,6 +102,9 @@ public class Basics {
          * Current count: 0
          */
 
+        for (int i = 10; i >=0; i--) {
+            System.out.println("Current count: " + i);
+        }
 
     }
 
@@ -143,6 +148,10 @@ public class Basics {
         StringBuilder ret = new StringBuilder();
 
         // Fill in the rest of the body here
+        String[] words = to_split.split(" ");
+        for (String word : words) {
+            ret.append(word.charAt(0));
+        }
 
         return ret.toString();
     }
@@ -170,6 +179,9 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+        for (int i = 1; i < arr.length; i += 2) {
+            current_sum += arr[i];
+        }
 
         return current_sum;
     }
